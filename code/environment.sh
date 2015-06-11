02e832ff1e56c8db4adf8e7a3bc3228db53a13c4
George Kastrinis <george.kast@gmail.com>
2015-06-11 21:34:28 +0300

    Adding query2 logic

--- code/environment.sh
+++ code/environment.sh
@@ -8,6 +8,6 @@ export LB_MEM="3G"
 export LB_MEM_NOWARN=1
 export LB_PAGER_FORCE_START=1
 export LOGICBLOX_HOME=${dir}/logicblox-3.10.14
-export PATH=${dir}/bin:$LOGICBLOX_HOME/bin:$PATH
+export PATH=${dir}/doop/tools/bin:$LOGICBLOX_HOME/bin:$PATH
 
-export DOOP_EXTERNALS=${dir}/benchmarks/JREs
+export DOOP_EXTERNALS=${dir}/doop/benchmarks/JREs
